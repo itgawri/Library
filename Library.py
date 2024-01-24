1ab75345ab714f13f0691d81cee34703e3ebaa1e
itgawri <136715144+itgawri@users.noreply.github.com>
2024-01-24 13:21:19 +0100

    Library.py

--- Library.py
+++ Library.py
@@ -18,26 +18,3 @@ is_my_author = "Ray Bradbury" in my_books_authors
 
 if is_my_author:
     print("Ray Bradbury belongs to my authors!")
-
-# PL-------------------------------------------------------------------
-
-informacje_o_hamlecie = ("William Shakespeare", "Tragedia", 1603)
-informacje_o_Kronikach_Marsa = ("Ray Bradbury", "Science fiction", 1950)
-moje_ksiazki = {"Hamlet": informacje_o_hamlecie, "Kroniki Marsa": informacje_o_Kronikach_Marsa}
-print(f"Moje książki: {moje_ksiazki}")
-
-moje_ksiazki["Fahrenheit 451"] = ("Ray Bradbury", "Dystopia", 1953)
-print(f"Informacje o Fahrenheit 451: {moje_ksiazki['Fahrenheit 451']}")
-
-autorzy_moich_ksiazek = set()
-
-for ksiazka in moje_ksiazki:
-    informacje_o_ksiazce = moje_ksiazki[ksiazka]
-    autor = informacje_o_ksiazce[0]
-    autorzy_moich_ksiazek.add(autor)
-print(f"Autorzy: {autorzy_moich_ksiazek}")
-
-czy_moj_autor = "Ray Bradbury" in autorzy_moich_ksiazek
-
-if czy_moj_autor:
-    print("Ray Bradbury należy do moich autorów!")
